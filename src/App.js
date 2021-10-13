9c676b636b7003aae4d541111d20c04f9953b45a
Pete Low <pedritolow@gmail.com>
2021-10-13 13:07:46 -0700

    added screens to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import AboutScreen from './Screens/AboutScreen';
+import FAQScreen from './Screens/FAQScreen';
+import ContactScreen from './Screens/ContactScreen';
+import HomeScreen from './Screens/HomeScreen';
 import Header from './Components/Header';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <Router>
       <Header />
+      <NavBar />
+      <Container>
+        <Route path='/' component={HomeScreen} exact />
+        <Route path='/about' exact component={AboutScreen} />
+        <Route path='/contact' exact component={ContactScreen} />
+        <Route path='/faq' exact component={FAQScreen} />
+      </Container>
     </Router>
   );
 }
