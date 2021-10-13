4bf1f5c7186d78d09cd15b63bcd595335dcbe326
Pete Low <pedritolow@gmail.com>
2021-10-13 14:47:12 -0700

    Homescreen image add

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { Col, Row, Image } from 'react-bootstrap';
+import Vaxed from '../Images/CovidVax.jpeg';
 const HomeScreen = () => {
   return (
     <div>
-      <h1>HomeScreen</h1>
+      <h1>Low Creations</h1>
+      <h2>Handyman and Repair Services</h2>
+      <Row>
+        <Col md={12}>
+          <Image src={Vaxed} alt='image of covid vaccine stickers' fluid />
+        </Col>
+      </Row>
     </div>
   );
 };
