9a67498f53120b38ab0248bf899b0a1e4d89710a
Pete Low <pedritolow@gmail.com>
2021-10-13 14:46:28 -0700

    Work gallery to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutScreen from './Screens/AboutScreen';
 import FAQScreen from './Screens/FAQScreen';
 import ContactScreen from './Screens/ContactScreen';
 import HomeScreen from './Screens/HomeScreen';
+import WorkGalleryScreen from './Screens/WorkGalleryScreen';
 import Header from './Components/Header';
 import NavBar from './Components/NavBar';
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/about' exact component={AboutScreen} />
         <Route path='/contact' exact component={ContactScreen} />
         <Route path='/faq' exact component={FAQScreen} />
+        <Route path='/work' exact component={WorkGalleryScreen} />
       </Container>
     </Router>
   );
