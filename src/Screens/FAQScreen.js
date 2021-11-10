29b8984217b9804bd24ccb936a288ffb7acbd287
Pete Low <pedritolow@gmail.com>
2021-11-09 21:08:51 -0800

    added content to FAQ

--- src/Screens/FAQScreen.js
+++ src/Screens/FAQScreen.js
@@ -3,7 +3,17 @@ import React from 'react';
 const FAQScreen = () => {
   return (
     <div>
-      <h1>FAQ</h1>
+      <h1>Frequently Asked Questions</h1>
+      <p>Q: Why is the first hour more expensive?</p>
+      <p>A: </p>
+      <p>Q: What areas of Seattle do you work in?</p>
+      <p>A: We're located in the Central District and mostly work in the surrounding area but feel free to inquire regarless of where you are located</p>
+      <p>Q: Do you handle plumbing?</p>
+      <p>A: It depends. Small jobs involving plumbing we can probably handle. If it is beyond our capabilities we can refer you to other local companies.</p>
+      <p>Q: Do you handle electrical work?</p>
+      <p>A: Again it depends. Smaller electrical jobs we can often handle. If it is beyond our capabilities we can refer you to other local companies.</p>
+      <p>Q: Why is the hourly rate higher for electrical and plumbing work?</p>
+      <p>A: Dealing with plumbing and electrical work requires a higher level of skill and exposure to risk. </p>
     </div>
   );
 };
