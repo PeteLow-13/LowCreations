5213eb017fe44fa740c88795f64ff1bb6b33f509
Pete Low <pedritolow@gmail.com>
2021-11-09 20:59:52 -0800

    created contact form

---
+++ src/Components/ContactForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+
+const ContactForm = () => {
+  return (
+    <>
+      <Form>
+        <Form.Group controlId='name'>
+          <Form.Label className='form-label'>Your Name</Form.Label>
+          <Form.Control type='name' placeholder='Your name' />
+        </Form.Group>
+
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Your Email</Form.Label>
+          <Form.Control type='email' placeholder='Your email' />
+          <Form.Text className='text-muted'>
+            {/* We'll never share your email with anyone else. */}
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId='exampleForm.ControlTextarea1'>
+          <Form.Label>Message</Form.Label>
+          <Form.Control as='textarea' rows={3} placeholder='Your Message' />
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>
+          Send
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default ContactForm;
