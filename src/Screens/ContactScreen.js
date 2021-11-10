da1459ba00c77456f7eb12ec7579ccb0b87287b4
Pete Low <pedritolow@gmail.com>
2021-11-09 21:08:40 -0800

    updated contact screen

--- src/Screens/ContactScreen.js
+++ src/Screens/ContactScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ContactForm from '../Components/ContactForm';
 
 const ContactScreen = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <>
+      <h1>Contact Us</h1>
+      <ContactForm />
+      <h3>Or Call<a href='tel:206-617-8254'>206-617-8254</a> </h3>
+    </>
   );
 };
 
