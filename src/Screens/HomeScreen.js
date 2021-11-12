32fceefbff19bd0f489720f7a7b294c6eb58a1bd
Pete Low <pedritolow@gmail.com>
2021-11-11 21:30:13 -0800

    added cards with info

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,18 +1,100 @@
 import React from 'react';
-import { Col, Row, Image } from 'react-bootstrap';
+import { Col, Row, Image, Card, Button } from 'react-bootstrap';
 import HomepageCarousel from '../Components/HomepageCarousel';
+import lookout from '../Images/leschiLookout.jpeg'
+import sticker from '../Images/vaccineSticker.png'
+import fence from '../Images/nunnFence2.jpeg'
+import plumbing from '../Images/plumbingTools.jpeg'
+import electric from '../Images/electricalTools.jpeg'
+import cabin from '../Images/cabinDeckAfter.jpeg'
+import safe from '../Images/covidSafe.jpeg'
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>Low Creations</h1>
-      <h2>Handyman and Repair Services</h2>
+    <>
       <Row>
         <Col md={12}>
-          <HomepageCarousel />
+          <Image src={lookout} alt='seattle' fluid/>
         </Col>
       </Row>
-    </div>
+      <h1>Low Creations</h1>
+      <h3>Handyman and Repair Services</h3>
+      <Row>
+        <Col md={6}>
+        <Card style={{ width: 'auto' }} className='rounded'>
+          <Card.Img variant="top" src={plumbing}/>
+          <Card.Body>
+            <Card.Title>Plumbing</Card.Title>
+            <Card.Text>
+             Got a job that's too much for you and not enough for a plumber? Talk to us about it.
+            </Card.Text>
+          </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+        <Card style={{ width: 'auto' }} className='rounded'>
+          <Card.Img variant="top" src={electric}/>
+          <Card.Body>
+            <Card.Title>Minor Electrical</Card.Title>
+            <Card.Text>
+             Do you have an outlet or a light fixture you'd like to update? Let's talk about it.
+            </Card.Text>
+          </Card.Body>
+          </Card>
+        </Col>
+        </Row>
+        <Row>
+        <Col md={6}>
+        <Card style={{ width: 'auto' }} className='rounded'>
+          <Card.Img variant="top" src={cabin} className='fluid'/>
+          <Card.Body>
+            <Card.Title>Interior, Exterior or Deck painting</Card.Title>
+            <Card.Text>
+             Paint need a refresh? 
+            </Card.Text>
+          </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6}>
+        <Card style={{ width: 'auto' }} className='rounded'>
+          <Card.Img variant="top" src={fence}/>
+          <Card.Body>
+            <Card.Title>Custom Fences</Card.Title>
+            <Card.Text>
+             
+            </Card.Text>
+          </Card.Body>
+          </Card>
+        </Col>
+        </Row>
+        <Row>
+        {/* <Col md={6}>
+          <h4>So much to do?</h4>
+          <p>Other projects </p>
+        </Col> */}
+          <Col md={6}></Col>
+        <Col md={3}>
+        <Card style={{ width: '10rem' }}>
+          <Card.Img variant="top" src={safe}/>
+          {/* <Card.Body>
+            <Card.Title>Fully Vaccinated</Card.Title>
+            <Card.Text>
+              We take COVID safety very seriously.
+            </Card.Text>
+          </Card.Body> */}
+          </Card>
+          </Col>
+          <Col md={3}>
+          <Card style={{ width: '10rem' }}>
+            <Card.Body>
+            <Card.Title>Fully Vaccinated</Card.Title>
+            <Card.Text>
+              We take safety very seriously. All of our employees are vaccinated.
+            </Card.Text>
+          </Card.Body></Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
