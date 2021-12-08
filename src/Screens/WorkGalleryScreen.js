04d1ef68be206b47e2c18faeb0141eeda75b9491
Pete Low <pedritolow@gmail.com>
2021-12-07 22:11:53 -0800

    added new carousels to work screen

--- src/Screens/WorkGalleryScreen.js
+++ src/Screens/WorkGalleryScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
-
-import ColeCarousel from '../Components/ColeCarousel';
 import { Row, Col } from 'react-bootstrap';
+import CabinDeckCarousel from '../Components/CabinDeckCarousel';
+import ClearCedarFenceCarousel from '../Components/ClearCedarFenceCarousel';
+import ColeCarousel from '../Components/ColeCarousel';
+import MobileFencing from '../Components/MobileFencing';
 
 
  
@@ -12,8 +13,18 @@ const WorkGalleryScreen = () => {
       <h1>Work Gallery</h1>
       <Row>
         <Col sm={12} md={12} lg={6} xl={6}>
-      
-      <ColeCarousel />
+          <ColeCarousel />
+        </Col>
+        <Col sm={12} md={12} lg={6} xl={6}>
+          <CabinDeckCarousel />
+        </Col>
+      </Row>
+      <Row>
+      <Col sm={12} md={12} lg={6} xl={6}>
+        <MobileFencing />
+      </Col>
+      <Col sm={12} md={12} lg={6} xl={6}>
+        <ClearCedarFenceCarousel />
       </Col>
       </Row>
     </>
