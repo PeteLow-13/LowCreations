883d9c0e3421768b34486500704a216d69a73454
Pete Low <pedritolow@gmail.com>
2021-12-07 19:42:11 -0800

    Created coleCarousel

---
+++ src/Components/ColeCarousel.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Carousel, Image, Card, Col } from 'react-bootstrap';
+import coleBefore from '../Images/coleStepsBefore.jpeg'
+import coleAfter from '../Images/coleStepsAfter.jpeg'
+
+const ColeCarousel = () => {
+    return (
+      <>
+      <Col md={6}>
+        <Card style={{ width: 'auto' }} className='rounded'>
+          <Carousel fade>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src={coleBefore}
+            alt="Picture of janky handrail"
+          />
+          <Carousel.Caption>
+            <h3 className='text-color-white'>Before</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src={coleAfter}
+            alt="Picture of new handrail"
+          />
+
+          <Carousel.Caption>
+            <h3>After</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        
+      </Carousel>
+          <Card.Body>
+            <Card.Title>Deck refresh</Card.Title>
+            <Card.Text>
+             New paint and handrail bringing it up to code.
+            </Card.Text>
+          </Card.Body>
+          </Card>
+        </Col>
+    </>
+    )
+}
+
+export default ColeCarousel
