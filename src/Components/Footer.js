aedba1ec5330624b5d7bd49fab91fd68a106226b
Pete Low <pedritolow@gmail.com>
2022-04-18 13:32:03 -0700

    Deleted uneeded code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,46 +1,13 @@
 import React from 'react';
 import './Footer.css';
-import * as FaIcons from 'react-icons/fa';
+
 import { Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   return (
     <>
       <footer className='mainfooter' role='contentinfo'>
-        {/* <div class="footer-middle"> */}
         <div className='container'>
-          <Row>
-            <Col className='col-md-3 col-sm-6'>
-              {/* <!--Column1--> */}
-              <a href='/faq'>FAQ</a>
-            </Col>
-
-            <Col className='col-md-3 col-sm-6'>
-              {/* <!--Column1--> */}
-              <a href='/about'>about us</a>
-            </Col>
-            <Col className='col-md-3 col-sm-6'>
-              {/* <!--Column1--> */}
-              <a href='/contact'>contact</a>
-            </Col>
-            <Col className='col-md-3 col-sm-6'>
-              {/* <!--Column1--> */}
-
-              <h4>follow us</h4>
-              <ul className='social-network social-circle'>
-                <li>
-                  <a href='#' className='icoFacebook' title='Facebook'>
-                    <i className='fa fa-facebook'></i>
-                  </a>
-                </li>
-                <li>
-                  <a href='#' className='icoLinkedin' title='Linkedin'>
-                    <i className='fa fa-linkedin'></i>
-                  </a>
-                </li>
-              </ul>
-            </Col>
-          </Row>
           <Row>
             <Col className='col-md-12 copy'>
               <p className='text-center'>
