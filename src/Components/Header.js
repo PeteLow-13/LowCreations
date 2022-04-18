08d360e00e103b9c7ac2f2c942d44d3213681020
Pete Low <pedritolow@gmail.com>
2022-04-18 13:32:29 -0700

    single '

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
-
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+    <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
       <Container>
-      <Navbar.Brand href="/">Low Creations</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="/about">About Us</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-          <Nav.Link href="/work">Our Work</Nav.Link>
-          <Nav.Link href="/faq">FAQ</Nav.Link>
-          {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+        <Navbar.Brand href='/'>LC</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            {/* <Nav.Link href='/about'>About Us</Nav.Link> */}
+            <Nav.Link href='/contact'>Contact</Nav.Link>
+            <Nav.Link href='/work'>Our Work</Nav.Link>
+            <Nav.Link href='/faq'>FAQ</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
             
           </NavDropdown> */}
-        </Nav>
-        
-      </Navbar.Collapse>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
