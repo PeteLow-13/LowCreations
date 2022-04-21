9eef3d99b65443fe0db611618bf9685d1f93c5f6
Pete Low <pedritolow@gmail.com>
2022-04-21 13:33:20 -0700

    Moved casousels

--- src/Screens/WorkGalleryScreen.js
+++ src/Screens/WorkGalleryScreen.js
@@ -5,27 +5,26 @@ import ClearCedarFenceCarousel from '../Components/ClearCedarFenceCarousel';
 import ColeCarousel from '../Components/ColeCarousel';
 import MobileFencing from '../Components/MobileFencing';
 
-
- 
 const WorkGalleryScreen = () => {
   return (
     <>
       <h1>Work Gallery</h1>
+      <h2>Just a small sample of projects we've done</h2>
       <Row>
+        <Col sm={12} md={12} lg={6} xl={6}>
+          <ClearCedarFenceCarousel />
+        </Col>
         <Col sm={12} md={12} lg={6} xl={6}>
           <ColeCarousel />
         </Col>
+      </Row>
+      <Row>
         <Col sm={12} md={12} lg={6} xl={6}>
           <CabinDeckCarousel />
         </Col>
-      </Row>
-      <Row>
-      <Col sm={12} md={12} lg={6} xl={6}>
-        <MobileFencing />
-      </Col>
-      <Col sm={12} md={12} lg={6} xl={6}>
-        <ClearCedarFenceCarousel />
-      </Col>
+        <Col sm={12} md={12} lg={6} xl={6}>
+          <MobileFencing />
+        </Col>
       </Row>
     </>
   );
