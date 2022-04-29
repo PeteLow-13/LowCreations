74272ab30663ddcab10fc08eb3ab25f8f3566630
Pete Low <pedritolow@gmail.com>
2022-04-28 19:19:51 -0700

    fixed routing isasue in header usinf LinkContainer

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
       <Container>
-        <Navbar.Brand href='/'>LC</Navbar.Brand>
+        <LinkContainer to='/'>
+        <Navbar.Brand >LC</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav className='me-auto'>
-            {/* <Nav.Link href='/about'>About Us</Nav.Link> */}
-            <Nav.Link href='/contact'>Contact</Nav.Link>
-            <Nav.Link href='/work'>Our Work</Nav.Link>
-            <Nav.Link href='/faq'>FAQ</Nav.Link>
+            
+            <LinkContainer to='/contact' >
+            <Nav.Link >Contact</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to='/work' >
+            <Nav.Link >Our Work</Nav.Link>
+            </LinkContainer>
+            
+            <LinkContainer to='/faq' >
+            <Nav.Link >FAQ</Nav.Link>
+            </LinkContainer>
+            
             {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
             
           </NavDropdown> */}
